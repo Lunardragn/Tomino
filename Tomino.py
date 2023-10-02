4e6fa899db53c6e5b943a7bb373563163c2c7c2d
Lunar <lvaserveremail@gmail.com>
2023-10-02 12:13:07 -0700

    Update Tomino.py

--- Tomino.py
+++ Tomino.py
@@ -35,6 +35,143 @@ text = """
 針の御山（おやま）の留針（とめはり）を。
 赤い留針だてにはささぬ、 
 可愛いトミノのめじるしに。
+
+Tomino’s Hell
+
+Elder sister vomits blood,
+younger sister’s breathing fire
+while sweet little Tomino
+just spits up the jewels.
+
+All alone does Tomino
+go falling into that hell,
+a hell of utter darkness,
+without even flowers.
+
+Is Tomino’s big sister
+the one who whips him?
+The purpose of the scourging
+hangs dark in his mind.
+
+Lashing and thrashing him, ah!
+But never quite shattering.
+One sure path to Avici,
+the eternal hell.
+
+Into that blackest of hells
+guide him now, I pray—
+to the golden sheep,
+to the nightingale.
+
+How much did he put
+in that leather pouch
+to prepare for his trek to
+the eternal hell?
+
+Spring is coming
+to the valley, to the wood,
+to the spiraling chasms
+of the blackest hell.
+
+The nightingale in her cage,
+the sheep aboard the wagon,
+and tears well up in the eyes
+of sweet little Tomino.
+
+Sing, o nightingale,
+in the vast, misty forest—
+he screams he only misses
+his little sister.
+
+His wailing desperation
+echoes throughout hell—
+a fox peony
+opens its golden petals.
+
+Down past the seven mountains
+and seven rivers of hell—
+the solitary journey
+of sweet little Tomino.
+
+If in this hell they be found,
+may they then come to me, please,
+those sharp spikes of punishment
+from Needle Mountain.
+
+Not just on some empty whim
+Is flesh pierced with blood-red pins:
+they serve as hellish signposts
+for sweet little Tomino.
+"""
+
+text2 = """
+
+Tomino’s Hell
+
+Elder sister vomits blood,
+younger sister’s breathing fire
+while sweet little Tomino
+just spits up the jewels.
+
+All alone does Tomino
+go falling into that hell,
+a hell of utter darkness,
+without even flowers.
+
+Is Tomino’s big sister
+the one who whips him?
+The purpose of the scourging
+hangs dark in his mind.
+
+Lashing and thrashing him, ah!
+But never quite shattering.
+One sure path to Avici,
+the eternal hell.
+
+Into that blackest of hells
+guide him now, I pray—
+to the golden sheep,
+to the nightingale.
+
+How much did he put
+in that leather pouch
+to prepare for his trek to
+the eternal hell?
+
+Spring is coming
+to the valley, to the wood,
+to the spiraling chasms
+of the blackest hell.
+
+The nightingale in her cage,
+the sheep aboard the wagon,
+and tears well up in the eyes
+of sweet little Tomino.
+
+Sing, o nightingale,
+in the vast, misty forest—
+he screams he only misses
+his little sister.
+
+His wailing desperation
+echoes throughout hell—
+a fox peony
+opens its golden petals.
+
+Down past the seven mountains
+and seven rivers of hell—
+the solitary journey
+of sweet little Tomino.
+
+If in this hell they be found,
+may they then come to me, please,
+those sharp spikes of punishment
+from Needle Mountain.
+
+Not just on some empty whim
+Is flesh pierced with blood-red pins:
+they serve as hellish signposts
+for sweet little Tomino.
 """
 # ANSI escape code for red text
 RED_TEXT = "\033[31m"
@@ -55,6 +192,13 @@ pygame.mixer.music.play()
 
 # Iterate through characters, color them red, and print to the terminal
 for char in text:
+    sys.stdout.write(RED_TEXT + char + RESET_COLOR)  # Print character in red
+    sys.stdout.flush()      # Flush buffer to ensure immediate display
+    time.sleep(0.01)         # Delay for a short duration
+print("\n")
+
+# Iterate through characters, color them red, and print to the terminal
+for char in text2:
     sys.stdout.write(RED_TEXT + char + RESET_COLOR)  # Print character in red
     sys.stdout.flush()      # Flush buffer to ensure immediate display
     time.sleep(0.01)         # Delay for a short duration
